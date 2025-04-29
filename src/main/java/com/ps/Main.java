900f12118959dd919747157cd3657af028269e4c
ozzie <ocarreon@appdev.yearup.org>
2025-04-29 09:06:52 -0400

    first push

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,13 @@
+package com.ps;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Welcome!");
+
+
+    }
+}
---
+++ src/main/java/com/ps/Transaction.java
@@ -0,0 +1,74 @@
+package com.ps;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private BigDecimal amount;
+
+    public Transaction(BigDecimal amount, String vendor, String description, LocalTime time, LocalDate date) {
+        this.amount = amount;
+        this.vendor = vendor;
+        this.description = description;
+        this.time = time;
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date=" + date +
+                ", time=" + time +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
+
+//date|time|description|vendor|amount
